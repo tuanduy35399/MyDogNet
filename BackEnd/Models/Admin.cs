6f0cb923e65c9d81a4ae2eb98808e519a48553cd
Duy <gochoctapcuatuanduy@gmail.com>
2026-04-26 11:41:44 +0700

    create: model Admin and Post

---
+++ BackEnd/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEnd.Models
+{
+    [Table("Admins")]
+    public class Admin
+    {
+        [Key]
+        [Required]
+        private int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Describe { get; set; }
+        public string Avatar { get; set; }
+        public string BackGroundImg { get; set; }
+
+        public List<Post> MyProperty { get; set; }
+        public bool VerifyPassword { get; set; }
+    }
+}
---
+++ BackEnd/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEnd.Models
+{
+    [Table("Posts")]
+    public class Post
+    {
+        [Key]
+        [Required]
+        private int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public List<string> ImageUrls { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        [Required]
+        private bool IsPublish { get; set; } = false;
+        public int ViewCount { get; set; }
+
+        protected void IncreaseView() { ViewCount++; }
+        protected bool Publish() => IsPublish = true;
+
+    }
+}
