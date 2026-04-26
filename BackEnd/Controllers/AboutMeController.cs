a003eb0689d1390f5698325b1e7ff166c897501f
Duy <gochoctapcuatuanduy@gmail.com>
2026-04-26 15:55:32 +0700

    update: create get endpoint

--- BackEnd/Controllers/AboutMeController.cs
+++ BackEnd/Controllers/AboutMeController.cs
@@ -1,10 +1,13 @@
 ﻿using BackEnd.Data;
+using BackEnd.DTOs.AdminDTOs;
+using BackEnd.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class AboutMeController : ControllerBase
     {
@@ -15,9 +18,40 @@ namespace BackEnd.Controllers
             _context = context;
         }
 
-        ////Tạo các endpoint
-        //[HttpGet]
-        //[ResponseCache(Location = ResponseCacheLocation.Client, Duration = 60)]
-        //public async 
+        //Tạo các endpoint
+        [HttpGet]
+        [ResponseCache(Location = ResponseCacheLocation.Client, Duration = 60)]
+        public async Task<IActionResult> GetDetailAboutMe()
+        {
+            var detail = await _context.Admins.Select(
+                admin => new ReadAdminDTO
+                {
+                    Name = admin.Name,
+                    Avatar = admin.Avatar,
+                    Describe = admin.Describe,
+                    BackGroundImg = admin.BackGroundImg,
+                }).ToArrayAsync();
+
+            return Ok(detail);
+        }
+        [HttpPost]
+        public async Task { get; set; }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Admin newAdmin)
+        {
+            if(newAdmin==null)
+            {
+                return BadRequest();
+            }
+            var oldAdmin = await _context.Admins.FindAsync(id);
+            if (oldAdmin == null) return NotFound();
+            oldAdmin.Name = newAdmin.Name;
+            oldAdmin.Avatar = newAdmin.Avatar;
+            oldAdmin.Describe = newAdmin.Describe;
+            oldAdmin.BackGroundImg = newAdmin.BackGroundImg;
+            _context.SaveChangesAsync();
+            return NoContent();
+        }
+
     }
 }
---
+++ BackEnd/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿//using BackEnd.Services;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace BackEnd.Controllers
+//{
+//    [Route("[controller]")]
+//    [ApiController]
+//    public class UploadController : ControllerBase
+//    {
+//        private readonly CloudinaryService _cloudinarySerivce;
+//            public UploadController(CloudinaryService cloudinaryService)
+//            {
+//                _cloudinarySerivce = cloudinaryService;
+//            }
+//            [HttpPost("upload")]
+//            public async Task<IActionResult> Upload(IFormFile file)
+//            {
+//            var url = await _cloudinarySerivce.UploadImageAsync(file);
+//            return Ok(new { url });
+//            }
+//        }
+
+//    }
+//}
