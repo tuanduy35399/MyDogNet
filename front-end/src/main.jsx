021c70f79af161747f26a84a6f995495fd663666
Duy <gochoctapcuatuanduy@gmail.com>
2026-05-10 22:52:50 +0700

    update: background

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,8 +5,8 @@ import App from './App.jsx'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+      <App />
   </StrictMode>,
-)
+);
