04e58f66e8b8a4b4fb442d2828520ae7f05c04a7
Duy <gochoctapcuatuanduy@gmail.com>
2026-05-28 22:39:22 +0700

    add analytics

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { inject } from "@vercel/analytics";
+
+//Kích hoạt Analytics
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <App />
+    <App />
   </StrictMode>,
 );
